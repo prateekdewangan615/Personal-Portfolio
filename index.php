3689607e5a6e1ab7ad1eb8f29fe4667d10f3438b
prateekdewangan615 <121946248+prateekdewangan615@users.noreply.github.com>
2023-08-12 20:41:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$con = mysqli_connect($server,$username, $password);
+$dbname = "portfolio";
+$con = mysqli_connect($server,$username,$password,$dbname);
 if(!$con){
     die("Connection failed: ".mysqli_connect_error());
 }
@@ -11,6 +12,20 @@ echo "Connection established successfully.";
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$sql = "INSERT INTO `portfolio`.`portfoliodata` (`name`, `email`, `message`) VALUES ('$name', '$email', '$message');";
+$sql = "INSERT INTO `portfoliodata` (`name`, `email`, `message`) VALUES ('$name', '$email', '$message');";
 echo $sql;
+echo "<br><br>";
+echo "The following message has been recorded";
+echo "<br>";
+echo "Name  : $name <br>";
+echo "Email  : $email <br>";
+echo "Message  : $message <br>";
+echo "<br>";
+if($con->query($sql)==true){
+    echo "Submitted Successfully";
+}        
+else{
+    echo "ERROR $sql <br> $con->error"; 
+}
+$con->close();
 ?>
